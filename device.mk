3e499ed2d745c42c4546c540e84159bf94c9815d
Reinascape <reinascape@gmail.com>
2026-01-10 23:15:50 +0300

    avalon: overlay: EvolutionX OTA-updates bringup
    Signed-off-by: Reinascape <reinascape@gmail.com>

--- device.mk
+++ device.mk
@@ -43,7 +43,8 @@ PRODUCT_PACKAGES += \
     OPlusSettingsResTarget \
     OPlusSystemUIResTarget \
     OPlusWifiResTarget \
-    SettingsResAvalon
+    SettingsResAvalon \
+    UpdaterResAvalon
 
 # Regional properties
 PRODUCT_COPY_FILES += \
