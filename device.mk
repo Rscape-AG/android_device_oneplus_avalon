35500d6c9305d7ebd89ad1abc272d4b7ce6d1a57
Reinascape <reinascape@gmail.com>
2026-04-14 17:39:26 +0300

    avalon: Inherit Google Camera
    Signed-off-by: Reinascape <reinascape@gmail.com>

--- device.mk
+++ device.mk
@@ -29,6 +29,9 @@ $(call soong_config_set,libcameraservice,ext_lib,//$(LOCAL_PATH):libcameraservic
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display/displayconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630947214369728643.xml
 
+# Google Camera
+$(call inherit-product-if-exists, vendor/google/camera/config.mk)
+
 # LiveDisplay
 $(call soong_config_set_bool,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_SE,false)
 
